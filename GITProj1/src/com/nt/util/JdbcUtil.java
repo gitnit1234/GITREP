e582903ea527414fe0052336fa22ebc86aaea495
Dev2 <Dev2@gmail.com>
2019-03-31 09:22:02 +0530

    Initial Commit TL

---
+++ GITProj1/src/com/nt/util/JdbcUtil.java
@@ -0,0 +1,14 @@
+package com.nt.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcUtil {
+	
+	public     static Connection  getConnection(String url,String user,String pwd)throws SQLException{
+		
+	return  DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system", "manager");
+	}
+	
+}
